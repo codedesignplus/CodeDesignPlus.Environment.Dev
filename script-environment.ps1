db4bf91e11d2fe933959615cacb2fcbe1aa3ec5b
CodeDesignPlus <codedesignplus@outlook.com>
2025-01-15 08:12:19 -0500

    Refactor installation script to streamline package installation and improve error handling

--- script-environment.ps1
+++ script-environment.ps1
@@ -1,164 +1,127 @@
-#Requires -RunAsAdministrator
-# Configuración del script
+# Reference : https://github.com/DrBorg/YTScripts/blob/main/UsingWinget.ps1
 
-$ErrorActionPreference = "Stop" # Detener el script si hay un error
+#======================================================================
+# NO POWERSHELL WINDOW DURING THE INSTALL
+#======================================================================
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
 
-Write-Host "---------------------------------"
-Write-Host "STARTING INSTALLATION SCRIPT"
-Write-Host "---------------------------------"
-
-# Función para verificar si un programa está instalado (usando winget)
-function Confirm-WingetPackageInstalled {
-    param (
-        [string]$PackageId
-    )
-    try {
-        winget show $PackageId | Out-Null
-        return $true
-    } catch {
-        return $false
-    }
+#======================================================================
+# CHECK IF THE SCRIPT IS ELEVATED / ELEVATE IF NOT
+#======================================================================
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Exit
 }
 
-# Función para instalar un paquete usando winget
-function Install-WingetPackage {
-    param (
-        [string]$PackageId,
-        [string]$PackageName
-    )
-    if (-not (Confirm-WingetPackageInstalled -PackageId $PackageId)) {
-        Write-Host "Installing $PackageName..."
-        winget install $PackageId --disable-interactivity --silent
-    } else {
-        Write-Host "$PackageName is already installed."
-    }
+#======================================================================
+# TURN OFF PROGRESS BAR TO MAKE SCRIPT RUN FASTER
+#======================================================================
+
+$ProgressPreference = 'SilentlyContinue'
+
+#======================================================================
+# BYPASS EXECUTION POLICY TO ALLOW SCRIPT TO RUN
+#======================================================================
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# Check if winget is installed
+        Write-Host "Checking if Winget is Installed..."
+        if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
+            #Checks if winget executable exists and if the Windows Version is 1809 or higher
+            Write-Host "Winget Already Installed"
+        }
+        else {
+            #Gets the computer's information
+            $ComputerInfo = Get-ComputerInfo
+
+            #Gets the Windows Edition
+            $OSName = if ($ComputerInfo.OSName) {
+                $ComputerInfo.OSName
+            }else {
+                $ComputerInfo.WindowsProductName
+            }
+
+            if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
+                
+                Write-Host "Running Alternative Installer for LTSC/Server Editions"
+
+                # Switching to winget-install from PSGallery from asheroto
+                # Source: https://github.com/asheroto/winget-installer
+                
+                Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/winget.ps1 | iex | Out-Host" -WindowStyle Normal
+                
+            }
+            elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
+                #Checks if Windows Version is too old for winget
+                Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
+            }
+            else {
+                #Installing Winget from the Microsoft Store
+                Write-Host "Winget not found, installing it now."
+                Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+                $nid = (Get-Process AppInstaller).Id
+                Wait-Process -Id $nid
+                Write-Host "Winget Installed"
+            }
+        }
+
+# installing packages via winget
+Write-Host "Installing required packages..."
+$packages = @(
+    "Node.js",
+    "Microsoft.DotNet.SDK",
+    "Microsoft.VisualStudioCode",
+    "Git.Git",
+    "Postman.Postman",
+    "drawio.drawio",
+    "MongoDB.Compass",
+     "grafana.k6",
+     "Docker.DockerDesktop",
+    "HashiCorp.Vault",
+    "pnpm.pnpm",
+    "Yarn.Yarn",
+    "Yeoman.Yo"
+)
+
+foreach ($package in $packages) {
+    Write-Host "Installing $package..."
+    winget install $package -h --accept-package-agreements --accept-source-agreements
+    Write-Host "$package installed successfully."
 }
-
-# ---------------------------------
-# 1 - Installing Node.js LTS
-# ---------------------------------
-Write-Host "1 - Installing Node.js LTS"
-Install-WingetPackage -PackageId "OpenJS.NodeJS.LTS" -PackageName "Node.js LTS"
-Write-Host "Verifying Node.js installation..."
-node -v
-npm -v
-
-
-# ---------------------------------
-# 2 - Installing .NET SDK
-# ---------------------------------
-Write-Host "2 - Installing .NET SDK"
-Install-WingetPackage -PackageId "Microsoft.DotNet.SDK.9" -PackageName ".NET SDK"
-Write-Host "Verifying .NET SDK installation..."
-dotnet --version
-
-
-# ---------------------------------
-# 3 - Installing Visual Studio Code
-# ---------------------------------
-Write-Host "3 - Installing Visual Studio Code"
-Install-WingetPackage -PackageId "Microsoft.VisualStudioCode" -PackageName "Visual Studio Code"
-
-
-# ---------------------------------
-# 4 - Installing Git
-# ---------------------------------
-Write-Host "4 - Installing Git"
-Install-WingetPackage -PackageId "Git.Git" -PackageName "Git"
-
-
-# ---------------------------------
-# 5 - Installing Postman
-# ---------------------------------
-Write-Host "5 - Installing Postman"
-Install-WingetPackage -PackageId "Postman.Postman" -PackageName "Postman"
-
-# ---------------------------------
-# 6 - Installing Draw.io
-# ---------------------------------
-Write-Host "6 - Installing Draw.io"
-Install-WingetPackage -PackageId "JGraph.Draw" -PackageName "Draw.io"
-
-# ---------------------------------
-# 7 - Installing MongoDB Compass
-# ---------------------------------
-Write-Host "7 - Installing MongoDB Compass"
-Install-WingetPackage -PackageId "MongoDB.Compass.Full" -PackageName "MongoDB Compass"
-
-# ---------------------------------
-# 8 - Installing K6
-# ---------------------------------
-Write-Host "8 - Installing K6"
-Install-WingetPackage -PackageId "k6.k6" -PackageName "K6"
-
-# ---------------------------------
-# 9 - Installing Docker Desktop
-# ---------------------------------
-Write-Host "9 - Installing Docker Desktop"
-Install-WingetPackage -PackageId "Docker.DockerDesktop" -PackageName "Docker Desktop"
-
-# ---------------------------------
-# 10 - Installing Hashicorp Vault
-# ---------------------------------
-Write-Host "10 - Installing Hashicorp Vault"
-Install-WingetPackage -PackageId "Hashicorp.Vault" -PackageName "Hashicorp Vault"
-
-# ---------------------------------
-# 11 - Installing pnpm
-# ---------------------------------
-Write-Host "11 - Installing pnpm"
-Install-WingetPackage -PackageId "pnpm.pnpm" -PackageName "pnpm"
-
-# ---------------------------------
-# 12 - Installing Yarn
-# ---------------------------------
-Write-Host "12 - Installing Yarn"
-Install-WingetPackage -PackageId "Yarn.Yarn" -PackageName "Yarn"
-
-
-# ---------------------------------
-# 13 - Installing Yeoman
-# ---------------------------------
-Write-Host "13 - Installing Yeoman"
-if (-not (Get-Command yo -ErrorAction SilentlyContinue)) {
-  Write-Host "Installing Yeoman (yo) globally..."
-  yarn global add yo
-} else {
-    Write-Host "Yeoman (yo) is already installed."
-}
-
 
 # ---------------------------------
 # 14 - Installing Extensions for Visual Studio Code
 # ---------------------------------
 Write-Host "14 - Installing Extensions for Visual Studio Code"
 $extensions = @(
-    "github.codespaces",
-    "github.copilot",
-    "github.copilot-chat",
-    "github.vscode-github-actions",
-    "ms-dotnettools.csdevkit",
-    "ms-dotnettools.csharp",
-    "ms-dotnettools.vscode-dotnet-runtime",
-    "ms-vscode.cpptools",
-    "ms-vscode.cpptools-extension-pack",
-    "ms-vscode.cpptools-themes",
-    "ms-vscode.test-adapter-converter",
-    "pkief.material-icon-theme",
-    "vivaxy.vscode-conventional-commits"
+  "github.codespaces",
+  "github.copilot",
+  "github.copilot-chat",
+  "github.vscode-github-actions",
+  "ms-dotnettools.csdevkit",
+  "ms-dotnettools.csharp",
+  "ms-dotnettools.vscode-dotnet-runtime",
+  "ms-vscode.cpptools",
+  "ms-vscode.cpptools-extension-pack",
+  "ms-vscode.cpptools-themes",
+  "ms-vscode.test-adapter-converter",
+  "pkief.material-icon-theme",
+  "vivaxy.vscode-conventional-commits"
 )
 
 foreach ($extension in $extensions) {
-    Write-Host "Installing extension: $extension"
-     if (-not (code --list-extensions | Where-Object { $_ -eq $extension })) {
-      code --install-extension $extension
-      Write-Host "Extension: $extension installed correctly"
-    } else{
-         Write-Host "Extension: $extension is already installed"
-    }
+  Write-Host "Installing extension: $extension"
+   if (-not (code --list-extensions | Where-Object { $_ -eq $extension })) {
+    code --install-extension $extension
+    Write-Host "Extension: $extension installed correctly"
+  } else{
+       Write-Host "Extension: $extension is already installed"
+  }
 }
 
-
-Write-Host "---------------------------------"
-Write-Host "INSTALLATION SCRIPT FINISHED"
-Write-Host "---------------------------------"
+Write-Host "Script execution completed."
